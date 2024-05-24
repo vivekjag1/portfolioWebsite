610dcac3323f5f7251a13513c014d78dbb42e931
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-05-24 00:18:47 -0400

    some planning for the home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,12 @@
-
+import betterVivekPhoto from "../assets/betterVivekPhoto.jpg"
 function Home(){
   return(
-    <div>Home</div>
+    // <div className="flex flex-row h-screen w-screen justify-center items-center mb-4">
+    //   <h1 className = "font-bold md:text-5xl font-poppins">Hello, World!</h1>
+    //   <img src = {betterVivekPhoto}/>
+    // </div>
+    <div>Home </div>
+
   )
 }
 export default Home;
