1f4f10f38c207f31702f70e9bead7143943d7b59
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-05-24 10:25:15 -0400

    fix font sizing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,9 +10,9 @@ export default function NavBar(){
   return(
     <nav className = "flex justify-between p-5 bg-white	 items-center items-baseline">
       <Link to = "/" className="text-4xl  text-black font-bold inline-block align-middle">
-      <div className = "flex items-center">
-        <img src = {shellFull}  alt = "shellClipart" className="mr-2 mt-1 h-8"/>
-        <Link to = "/" className="text-4xl  text-black font-bold inline-block align-middle">Vivek Jagadeesh</Link>
+      <div className = "flex items-center flex-shrink-0 ">
+        <img src = {shellFull}  alt = "shellClipart" className="  mr-2 mt h-8"/>
+        <Link to = "/" className=" text-3xl  text-black font-bold  whitespace-nowrap">Vivek Jagadeesh</Link>
       </div>
       </Link>
       <ul className = " text-lg font-medium items-baseline text-black hidden md:flex gap-6 mb">
@@ -21,7 +21,7 @@ export default function NavBar(){
         <Link to = "/certifications" ><li>Certifications</li></Link>
       </ul>
       <div className="md:hidden z-10" onClick = {handleClickX}>
-        {mobileOpen? <IoMdClose size = {25} className="cursor-pointer text-black"/> : <RxHamburgerMenu size = {25}/>}
+        {mobileOpen? <IoMdClose size = {25} className="cursor-pointer text-black"/> : <RxHamburgerMenu size = {25} />}
       </div>
       <ul className={`${
         mobileOpen? 'text-black opacity-100 transform translate-x-0' : 'opacity-0 transform -translate-y-full'} transition-transform absolute top-0 left-0 w-full h-screen bg-white flex flex-col justify-center items-center text-2xl
