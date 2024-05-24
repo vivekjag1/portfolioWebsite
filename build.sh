2f52179c9c458dcae16bb7fee656c32ca610649d
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-05-24 00:33:38 -0400

    push for prod

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -am "push for prod"
 git push
-docker build -t portfilio .
-docker tag portfilio vivekjag1/portfilio
+docker build -t portfolio .
+docker tag portfolio vivekjag1/portfolio
 docker push vivekjag1/portfolio
