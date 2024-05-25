41b014ec0b1bff2356df009f43cbdcc863528907
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-05-25 16:29:12 -0400

    fix for mobile responsiveness being bad

--- TODO.md
+++ TODO.md
@@ -1,8 +1,7 @@
 <h1> <b> Need to fix ASAP </b> </h1>
-2. Responsiveness of the "Hello World" <br> </br>
-3. Responsiveness on mobile/small devices <br> </br>
 4. Add typing animation for the hello world <br> </br>
-5. Troubleshoot EC2 deployment <br> </br>
-6. Mostly issues with responsiveness and then the issue of adding the typing animation ;) <br> </br>
-7. Other pages :D <br></br>
+5. Custom footer <br></br>
+6. Troubleshoot EC2 deployment <br> </br>
+7. Mostly issues with responsiveness and then the issue of adding the typing animation ;) <br> </br>
+8. Other pages :D <br></br>
 
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,17 @@
 import vivekHeadshot from "../assets/vivekHeadshot.png"
+// import {TypeAnimation} from "react-type-animation";
 function Home(){
   return(
     <>
-      <section>
+      <section className = "hidden md:flex ">
         <div className="w-screen h-screen grid grid-cols-2 overflow-hidden ">
           <div className = "flex flex-col  items-center justify-center  h-full col-span-1 ">
-            <h1 className="text-black font-mono font-bold text-8xl">
+            <h1 className="text-black font-mono font-bold md:text-6xl lg:text-6xl">
               Hello,World!
             </h1>
             <br></br>
             <h1 className = "text-black font-semibold text-2xl font-arial  ml-10">
-              My name is Vivek Jagadeesh and I am a junior at Worcester Polytechnic Institute (WPI) studying Computer Science and minoring in Electrical and Computer Engineering.  My interests include Software Engineering, Systems Programming, and Cloud Computing. Visit the about page to learn more about me and my interests.
+              My name is Vivek Jagadeesh and I am a junior at Worcester Polytechnic Institute (WPI) majoring in Computer Science and minoring in Electrical and Computer Engineering.  My interests include Software Engineering, Systems Programming, and Cloud Computing. Visit the about page to learn more about me and my interests.
             </h1>
           </div>
           <div className = "flex items-center justify-center  h-full  col-span-1">
@@ -18,20 +19,34 @@ function Home(){
           </div>
         </div>
       </section>
+      <section className="flex md:hidden">
+      <div className = "flex flex-col sm:flex-row w-full">
+        <div className="w-full h-auto grid grid-row-2 py-10 sm:py-0">
+          <div className="flex flex-col items-center justify-center w-full">
+            <div className="flex flex-col items-center justify-center w-full">
+              <img src={vivekHeadshot} alt="vivek jagadeesh "/>
+              <h1
+                className="text-black text-center font-mono font-bold  text-4xl md:text-6xl lg:text-8xl mt-2 ">Hello,World!</h1>
+              <div className="w-full h-auto flex flex-col items-center justify-center   sm:ml-5">
+                <h1
+                  className="text-black font-semibold text-center text-xl sm:text-xl font-arial   overflow-x-hidden">
+                  My name is Vivek Jagadeesh and I am a junior at Worcester Polytechnic Institute (WPI) majoring in
+                  Computer Science and minoring in Electrical and Computer Engineering. My interests include Software
+                  Engineering, Systems Programming, and Cloud Computing. Visit the about page to learn more about me and
+                  my interests.
+                </h1>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      </section>
 
 
     </>
 
-
-
-
-    //  <div className="flex flex-row  justify-center items-center mb-4">
-    //
-    // <h1 className="font-bold md:text-5xl font-mono">Website Under Construction!</h1>
-    //  </div>
-
-
-
   )
 }
+
 export default Home;
