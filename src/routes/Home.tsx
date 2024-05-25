505db191b70616a62301a3a5a80412012e72ef3d
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-05-25 16:56:08 -0400

    type animation

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,18 @@
 import vivekHeadshot from "../assets/vivekHeadshot.png"
-// import {TypeAnimation} from "react-type-animation";
+import {TypeAnimation} from "react-type-animation";
+
 function Home(){
   return(
     <>
       <section className = "hidden md:flex ">
         <div className="w-screen h-screen grid grid-cols-2 overflow-hidden ">
           <div className = "flex flex-col  items-center justify-center  h-full col-span-1 ">
-            <h1 className="text-black font-mono font-bold md:text-6xl lg:text-6xl">
-              Hello,World!
-            </h1>
+            <TypeAnimation className="text-black font-mono font-bold md:text-6xl lg:text-6xl cursor-hidden" sequence={["Hello, World!"]}
+            />
+
+            {/*<h1 className="text-black font-mono font-bold md:text-6xl lg:text-6xl">*/}
+            {/*  Hello,World!*/}
+            {/*</h1>*/}
             <br></br>
             <h1 className = "text-black font-semibold text-2xl font-arial  ml-10">
               My name is Vivek Jagadeesh and I am a junior at Worcester Polytechnic Institute (WPI) majoring in Computer Science and minoring in Electrical and Computer Engineering.  My interests include Software Engineering, Systems Programming, and Cloud Computing. Visit the about page to learn more about me and my interests.
@@ -25,8 +29,8 @@ function Home(){
           <div className="flex flex-col items-center justify-center w-full">
             <div className="flex flex-col items-center justify-center w-full">
               <img src={vivekHeadshot} alt="vivek jagadeesh "/>
-              <h1
-                className="text-black text-center font-mono font-bold  text-4xl md:text-6xl lg:text-8xl mt-2 ">Hello,World!</h1>
+              <TypeAnimation
+                className="text-black text-center font-mono font-bold  text-4xl md:text-6xl lg:text-8xl mt-2 " sequence={["Hello, World!"]}/>
               <div className="w-full h-auto flex flex-col items-center justify-center   sm:ml-5">
                 <h1
                   className="text-black font-semibold text-center text-xl sm:text-xl font-arial   overflow-x-hidden">
