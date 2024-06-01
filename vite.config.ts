0c4207147d6e9c53d98ecda1558c3f10182eb3c1
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-01 09:46:54 -0400

    malformed uri fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
+    hmr: {
+      overlay: false,
+    },
     port: 80,
     strictPort: true,
     host: true,
+
+
   }
 });
