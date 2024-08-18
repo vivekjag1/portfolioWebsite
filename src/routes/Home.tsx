f73a6ba708bdcb84367f92391bca9dbf3a1c6620
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-08-18 17:02:07 -0400

    ui improvements pt 2

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,9 @@
 import vivekHeadshot from "../assets/Vivek-Picture-One.jpg"
 import {TypeAnimation} from "react-type-animation";
+import Button from '@mui/material/Button';
+import MonitorIcon from '@mui/icons-material/Monitor';
+import PersonIcon from '@mui/icons-material/Person';
+import {Stack} from "@mui/material";
 
 function Home(){
   return(
@@ -11,10 +15,28 @@ function Home(){
             />
             <br></br>
             <h1 className = "text-black font-semibold text-2xl font-arial  ml-20">
-              My name is Vivek Jagadeesh and I am a junior at Worcester Polytechnic Institute (WPI) majoring in Computer Science and minoring in Electrical and Computer Engineering.
-              My interests include Software Engineering, Embedded Systems, and Cloud Computing. Visit the about page to learn more about me and my interests!
-
+              I'm a junior at Worcester Polytechnic Institute (WPI) majoring in Computer Science with a minor in Electrical and Computer Engineering.
+              My interests include Software Engineering, Embedded Systems, and Cloud Computing. Use the links below to learn more about me and my interests!
             </h1>
+            <div className="hidden md:flex justify-center">
+
+            <Stack spacing={2} direction="row" className="items-center mt-7">
+              <Button href="/about"  variant="contained"
+                      startIcon={<PersonIcon/>}
+                      sx={{"&.MuiButtonBase-root:hover": {backgroundColor: "black"}, width: "10rem", bgcolor: 'black'}}
+                      className="mr-4 hover:bg-black"> About Me </Button>
+              <Button variant="contained" className=" text-white mx-auto" href="/projects"
+                      startIcon={<MonitorIcon/>} sx={{
+                "&.MuiButtonBase-root:hover": {backgroundColor: "black"},
+                width: "10rem",
+                bgcolor: 'black'
+              }}>Projects</Button>
+
+            </Stack>
+            </div>
+
+
+
           </div>
           <div className = "flex items-center justify-center  ml-20  h-fit w-11/12  col-span-1">
             <img src={vivekHeadshot} alt = "Vivek Jagadeesh" className="  max-h-[46rem] mt-5 ml-20 overflow-hidden"/>
@@ -32,9 +54,32 @@ function Home(){
                 <div className="w-full h-auto flex flex-col items-center justify-center   sm:ml-5">
                   <h1
                     className="text-xl mt-7 text-left md:w-1/2 ml-5 ">
-                    My name is Vivek Jagadeesh and I am a junior at Worcester Polytechnic Institute (WPI) majoring in Computer Science and minoring in Electrical and Computer Engineering.
-                    My interests include Software Engineering, Embedded Systems, and Cloud Computing. Visit the about page to learn more about me and my interests!
+                    I'm a junior at Worcester Polytechnic Institute (WPI) majoring in Computer Science with a minor in Electrical and Computer Engineering.
+                    My interests include Software Engineering, Embedded Systems, and Cloud Computing. Use the links below to learn more about me and my interests!
                   </h1>
+
+                  <div className="md:hidden sm:flex ">
+                    <div className="flex justify-center">
+                      <Stack spacing={2} direction="column" className="items-center mt-5">
+                        <Button href="/about"   variant="contained"
+                                startIcon={<PersonIcon/>}
+                                sx={{
+                                  "&.MuiButtonBase-root:hover": {backgroundColor: "black"},
+                                  width: "10rem",
+                                  bgcolor: 'black'
+                                }}
+                                className="mr-4 hover:bg-black"> About Me</Button>
+                        <Button variant="contained" className=" text-white mx-auto" href="https://brighamwomens.com"
+                                target="_blank"
+                                startIcon={<MonitorIcon/>} sx={{
+                          "&.MuiButtonBase-root:hover": {backgroundColor: "black"},
+                          width: "10rem",
+                          bgcolor: 'black'
+                        }}> Projects</Button>
+
+                      </Stack>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
