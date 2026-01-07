18dca5d2dcebb69949a62550b684d16abf12348d
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2026-01-07 13:05:44 -0500

    hotfix resume sizing

--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -1,11 +1,13 @@
 export default function ResumePage() {
   return (
     <div className=" w-[100vw] h-screen inset-0">
-      <iframe
+          <embed
         src="/Jagadeesh_V_Resume_Oct_2025.pdf"
-        className="w-full h-full border-none"
-        title="Resume"
+        className="w-full h-full  max-h-screen"
+        type="application/pdf"
+        style={{ objectFit: 'contain' }}
       />
+
     </div>
   );
 }
